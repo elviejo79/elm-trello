7153fd278a65bb4cf341e22e72fe4fe12043b61a
elviejo79 <agarciafdz@gmail.com>
2017-01-23 18:38:31 -0600

    Just o Login

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,24 @@
 module Main exposing (main)
 
-import Html exposing (Html)
-
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 main : Html Never
 main =
-    Html.div [] []
+    div [ class "container" ]
+        [ Html.form [ class "form-signin" ]
+              [ h2 [ class "form-signin-heading" ][ text "Please sign in" ]
+              , label [ class "sr-only", for "inputEmail" ][ text "Email address" ]
+              , input [ attribute "autofocus" "", class "form-control", id "inputEmail", placeholder "Email address", attribute "required" "", type_ "email" ][]
+              , label [ class "sr-only", for "inputPassword" ][ text "Password" ]
+              , input [ class "form-control", id "inputPassword", placeholder "Password", attribute "required" "", type_ "password" ][]
+              , div [ class "checkbox" ]
+                  [ label []
+                        [ input [ type_ "checkbox", value "remember-me" ][]
+                        , text "Remember me"
+                        ]
+                  ]
+              , button [ class "btn btn-lg btn-primary btn-block", type_ "submit" ][ text "Sign in" ]
+              ]
+        ]
+              
